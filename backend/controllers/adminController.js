7a0ef70e8347766ab5c9f8c7a5adfc9736a59d56
Aakamsh PM <aakamshpm@gmail.com>
2024-12-18 01:09:38 +0530

    admin controllers defined

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import asyncHandler from "express-async-handler";
 import bcrypt from "bcrypt";
 import generateToken from "../utils/generateToken.js";
 import Admin from "../models/Admin.js";
+import Student from "../models/Student.js";
 
 const authAdmin = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
@@ -33,4 +34,152 @@ const authAdmin = asyncHandler(async (req, res) => {
   }
 });
 
-export { authAdmin };
+const getStudentsAppliedForEligibility = asyncHandler(async (req, res) => {
+  try {
+    const students = await Student.find(
+      {
+        "eligibility.status": "pending",
+        "application.status": "false",
+      },
+      "firstName lastName dateOfBirth email address institutionDetails studentIdCard"
+    );
+    res.status(200).json({ data: students });
+  } catch (err) {
+    res.status(400);
+    throw new Error("Error fetching students for eligibility");
+  }
+});
+
+const getStudentsAppliedForApplication = asyncHandler(async (req, res) => {
+  try {
+    const students = await Student.find({
+      "application.status": "pending",
+      "eligibility.status": "approved",
+    }).select("-password");
+    res.status(200).json({ data: students });
+  } catch (err) {
+    res.status(400);
+    throw new Error("Error fetching students for eligibility");
+  }
+});
+
+const verifyStudentId = asyncHandler(async (req, res) => {
+  const { studentId, decision, reason } = req.body;
+
+  try {
+    if (!studentId) {
+      res.status(400);
+      throw new Error("Please provide the studentId");
+    }
+
+    const student = await Student.findByIdAndUpdate(studentId);
+
+    if (!student) {
+      res.status(404);
+      throw new Error("No student found");
+    }
+
+    if (student.eligibility.status === "false") {
+      res.status(400);
+      throw new Error("Student not applied for verification");
+    }
+
+    if (["approved", "rejected"].includes(student.eligibility.status)) {
+      res.status(400);
+      throw new Error("Student already verified by admins");
+    }
+
+    if (!decision) {
+      res.status(400);
+      throw new Error("Please select an option");
+    }
+
+    // If there is a decision, there should be a reason
+    if (decision === "rejected" && !reason) {
+      res.status(400);
+      throw new Error("Please provide a reason for rejection");
+    }
+
+    await Student.findByIdAndUpdate(
+      studentId,
+      { "eligibility.status": decision, "eligibility.reason": reason },
+      { new: true }
+    );
+
+    res.status(200).json({ message: "Verification successful" });
+  } catch (err) {
+    res.status(500);
+    throw new Error(err.message);
+  }
+});
+
+const approveStudentConcessionCard = asyncHandler(async (req, res) => {
+  const { studentId, decision, reason } = req.body;
+
+  try {
+    if (!studentId) {
+      res.status(400);
+      throw new Error("Please provide the studentId");
+    }
+
+    const student = await Student.findByIdAndUpdate(studentId);
+
+    if (!student) {
+      res.status(404);
+      throw new Error("No student found");
+    }
+
+    if (student.application.status === "false") {
+      res.status(400);
+      throw new Error("Student not applied for application");
+    }
+
+    if (student.application.status === "withdrawn") {
+      res.status(400);
+      throw new Error("Student application withdrawn");
+    }
+
+    if (["approved", "rejected"].includes(student.application.status)) {
+      res.status(400);
+      throw new Error("Student already verified by admins");
+    }
+
+    if (!decision) {
+      res.status(400);
+      throw new Error("Please select an option");
+    }
+
+    // If there is a decision, there should be a reason
+    if (decision === "rejected" && !reason) {
+      res.status(400);
+      throw new Error("Please provide a reason for rejection");
+    }
+
+    const concessionCardUrl = generateConcessionCard();
+
+    await Student.findByIdAndUpdate(
+      studentId,
+      {
+        concessionCardUrl,
+        "application.status": decision,
+        "application.reason": reason,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({ message: "Application verified" });
+  } catch (err) {
+    res.status(500);
+    throw new Error(err.message);
+  }
+});
+
+const generateConcessionCard = () => {};
+
+export {
+  authAdmin,
+  getStudentsAppliedForEligibility,
+  getStudentsAppliedForApplication,
+  verifyStudentId,
+  approveStudentConcessionCard,
+};
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,20 @@
 import express from "express";
-import { authAdmin } from "../controllers/adminController.js";
+import {
+  approveStudentConcessionCard,
+  authAdmin,
+  getStudentsAppliedForApplication,
+  getStudentsAppliedForEligibility,
+  verifyStudentId,
+} from "../controllers/adminController.js";
 
 const router = express.Router();
 
 router.post("/auth", authAdmin);
+
+router.get("/students-eligibility", getStudentsAppliedForEligibility);
+router.get("/students-application", getStudentsAppliedForApplication);
+
+router.post("/verify-student", verifyStudentId);
+router.post("/approve-concession", approveStudentConcessionCard);
 
 export default router;
